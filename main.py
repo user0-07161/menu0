d004265e9db8e0a8160e25646a1b25d445b87d60
User0 <104380885+user0-07161@users.noreply.github.com>
2024-10-12 22:45:31 +0200

    Add uh code

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import tkinter as tk
+from tkinter import ttk, simpledialog, filedialog
+import os 
+import json
+import subprocess
+from functools import partial
+
+root = tk.Tk()
+root.geometry("750x250")
+root.title("menu0")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+homedir = os.getenv("HOME")
+apps = {}
+root.configure(background='black')
+
+if not os.path.isfile(f"{homedir}/.menu0.json"):
+    with open(f"{homedir}/.menu0.json", 'w') as conf:
+        json.dump(apps, conf)
+else:
+    with open(f"{homedir}/.menu0.json", 'r') as conf:
+        try:
+            apps = json.load(conf)
+        except:
+            apps = {}
+
+
+def runApp(appl):
+    subprocess.Popen(appl.split(" "))
+def addApp():
+    name = simpledialog.askstring("Application name", "What's the application's name?", parent=root)
+    dir = filedialog.askopenfilename(parent=root, initialdir="/", title="Please select the program", filetypes=[('All files', '*')])
+    print(dir)
+    if name != "":
+        apps[name] = dir
+        with open(f"{homedir}/.menu0.json", 'w') as conf:
+            json.dump(apps, conf)
+def remConf():
+    with open(f"{homedir}/.menu0.json", 'w') as conf:
+        json.dump({}, conf)
+
+for app in apps:
+    button = tk.Button(root, text=app, width=20, height=1, command=lambda appl=apps[app]: runApp(appl), padx=10, pady=10)
+    button["border"] = 0
+    button.pack()
+    print(apps)
+
+button = tk.Button(root, text="Add app", width=20, height=1, command=addApp, padx=10, pady=10)
+button["border"] = 0
+button.pack()
+button2 = tk.Button(root, text="Reset config", width=20, height=1, command=remConf, padx=10, pady=10, border=0)
+button2.pack()
+
+root.mainloop()
